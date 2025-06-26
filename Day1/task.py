241538e68394e26f8e9b895b885383a8ba14832e
Jake <jakechalmers86@gmail.com>
2025-06-26 13:25:13 +0800

    Day1: Finding Length of User Input

--- Day1/task.py
+++ Day1/task.py
@@ -10,3 +10,9 @@ print("Hi," + " " + "Jake")
 # Combine print + input functions
 print("You are " + input("What is your age? ") + " " + "years old!") 
 
+# Assigning a string Variable
+name = "Johnny"
+print(name) 
+
+# Finding out len
+print(len(input("What is your name?")))
