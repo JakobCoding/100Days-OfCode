4bd8a21a28630c90e7cb0dedcadcd7b355e3c25b
Jake <jakechalmers86@gmail.com>
2025-06-26 14:26:22 +0800

    Day1: Completed Logic for BandNameGenerator

---
+++ Day1/project.py
@@ -0,0 +1,9 @@
+# Band Name Generator Program 
+
+print("Welcome to the Band Name Generator.")
+
+city = input("Whats the name of the city you grew up in? ")
+petname = input("What's your pet's name? ")
+bandName = city + " " + petname
+
+print("Your band name could be" + " " + bandName)
