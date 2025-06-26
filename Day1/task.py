6378da11ee097adafa7101e72e81a0f0ff7a1458
Jake <jakechalmers86@gmail.com>
2025-06-26 13:54:05 +0800

    Day1: Removing comments from Task.py

--- Day1/task.py
+++ Day1/task.py
@@ -1,27 +1,27 @@
-# # Strings
-# print("Hi")  
+# Strings
+print("Hi")  
 
-# # Input() function
-# input("What is your name? ")
+# Input() function
+input("What is your name? ")
 
-# # String concatenation 
-# print("Hi," + " " + "Jake")
+# String concatenation 
+print("Hi," + " " + "Jake")
 
-# # Combine print + input functions
-# print("You are " + input("What is your age? ") + " " + "years old!") 
+# Combine print + input functions
+print("You are " + input("What is your age? ") + " " + "years old!") 
 
-# # Assigning a string Variable
-# name = "Johnny"
-# print(name) 
+# Assigning a string Variable
+name = "Johnny"
+print(name) 
 
-# # Finding out len
-# print(len(input("What is your name?")))
+# Finding out len
+print(len(input("What is your name?")))
 
 # Storing input in variables 
-# username = input("What is your name? ")
-# length = len(username)
+username = input("What is your name? ")
+length = len(username)
 
-# print(length)
+print(length)
 
 # Challange Swap the contents of 2 variables without changing the names
 glass1 = "milk" 
