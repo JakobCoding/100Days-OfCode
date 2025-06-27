573a652a8ed1bda97e91a09597039d9cbaecce8b
Jake <jakechalmers86@gmail.com>
2025-06-27 20:25:06 +0800

    Day 2: Project (Tip Calculator: First Attempt)

--- Day2/project.py
+++ Day2/project.py
@@ -1,3 +1,18 @@
 # Build a Tip Calculator 
+# First Attempt 
 
 print("Welcome to the tip caluculator!")
+totalBill = input("What was the total bill? ")
+tip = input("How much tip would you like to give? 10, 12, or 15? ")
+splitBetween = input("How many People to split the bill? ")
+
+floatBill = float(totalBill)
+floatTip = float(tip)
+tipPercentage = floatTip / 100
+actualTip = floatBill * tipPercentage
+intSplitBetween = int(splitBetween)
+
+splitTip = (floatBill + actualTip) / intSplitBetween
+splitTipRound = round(splitTip, 2)
+
+print(f"Each person should pay: ${splitTipRound}")
