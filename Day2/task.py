6e5dea8559847f899cd9f1264d9da38cb6f7cd9e
Jake <jakechalmers86@gmail.com>
2025-06-27 18:49:57 +0800

    Day 2: Refined solution to Debug challange

--- Day2/task.py
+++ Day2/task.py
@@ -34,4 +34,7 @@ print(int("123") + int("435"))
 name_of_user = input("What is your name? \n")
 length_of_name = len(name_of_user)
 
+print(type(name_of_user)) # string
+print(type(length_of_name)) # int
+
 print("Number of letters in your name: " + str(length_of_name))
