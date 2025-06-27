b517e7cb77af26f7310706314a69130a5537e7da
Jake <jakechalmers86@gmail.com>
2025-06-27 20:50:47 +0800

    Day 2: Project (Tip Calculator: Second Attempt

--- Day2/project.py
+++ Day2/project.py
@@ -1,18 +1,31 @@
-# Build a Tip Calculator 
-# First Attempt 
+                # Build a Tip Calculator 
+                    # First Attempt 
+
+# print("Welcome to the tip caluculator!")
+# totalBill = input("What was the total bill? ")
+# tip = input("How much tip would you like to give? 10, 12, or 15? ")
+# splitBetween = input("How many People to split the bill? ")
+
+# floatBill = float(totalBill)
+# floatTip = float(tip)
+# tipPercentage = floatTip / 100
+# actualTip = floatBill * tipPercentage
+# intSplitBetween = int(splitBetween)
+
+# splitTip = (floatBill + actualTip) / intSplitBetween
+# splitTipRound = round(splitTip, 2)
+
+# print(f"Each person should pay: ${splitTipRound}")
+
+                    # Second Attempt #
 
 print("Welcome to the tip caluculator!")
-totalBill = input("What was the total bill? ")
-tip = input("How much tip would you like to give? 10, 12, or 15? ")
-splitBetween = input("How many People to split the bill? ")
 
-floatBill = float(totalBill)
-floatTip = float(tip)
-tipPercentage = floatTip / 100
-actualTip = floatBill * tipPercentage
-intSplitBetween = int(splitBetween)
+totalBill = float(input("What was the total bill? "))
+tip = float(input("How much tip would you like to give? 10, 12, or 15? "))
+splitBetween = float(input("How many People to split the bill? "))
 
-splitTip = (floatBill + actualTip) / intSplitBetween
-splitTipRound = round(splitTip, 2)
+tipPercentage = tip / 100 + 1 
+billWithTip = totalBill * tipPercentage / splitBetween
 
-print(f"Each person should pay: ${splitTipRound}")
+print(f"Each person should pay: ${round(billWithTip, 2)}")
