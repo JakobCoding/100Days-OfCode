0d7fa92277b9f62392bf66de5d64cda285975774
Jake <jakechalmers86@gmail.com>
2025-06-27 21:09:57 +0800

    Day 2: Project Final Tweaks

--- Day2/project.py
+++ Day2/project.py
@@ -22,7 +22,7 @@
 print("Welcome to the tip caluculator!")
 
 totalBill = float(input("What was the total bill? $"))
-tip = float(input("How much tip would you like to give? 10, 12, or 15? "))
+tip = float(input("What percentage tip would you like to give? 10, 12, or 15? "))
 splitBetween = float(input("How many People to split the bill? "))
 
 tipPercentage = tip / 100 + 1 
