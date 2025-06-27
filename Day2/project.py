6d1a8c34a059f8145a4f26170fc812ea3a16f67a
Jake <jakechalmers86@gmail.com>
2025-06-27 21:15:58 +0800

    Day 2: Project Final Tweaks

--- Day2/project.py
+++ Day2/project.py
@@ -1,21 +1,21 @@
                 # Build a Tip Calculator 
                     # First Attempt 
 
-# print("Welcome to the tip caluculator!")
-# totalBill = input("What was the total bill? ")
-# tip = input("How much tip would you like to give? 10, 12, or 15? ")
-# splitBetween = input("How many People to split the bill? ")
+print("Welcome to the tip caluculator!")
+totalBill = input("What was the total bill? ")
+tip = input("How much tip would you like to give? 10, 12, or 15? $")
+splitBetween = input("How many People to split the bill? ")
 
-# floatBill = float(totalBill)
-# floatTip = float(tip)
-# tipPercentage = floatTip / 100
-# actualTip = floatBill * tipPercentage
-# intSplitBetween = int(splitBetween)
+floatBill = float(totalBill)
+floatTip = float(tip)
+tipPercentage = floatTip / 100
+actualTip = floatBill * tipPercentage
+intSplitBetween = int(splitBetween)
 
-# splitTip = (floatBill + actualTip) / intSplitBetween
-# splitTipRound = round(splitTip, 2)
+splitTip = (floatBill + actualTip) / intSplitBetween
+splitTipRound = round(splitTip, 2)
 
-# print(f"Each person should pay: ${splitTipRound}")
+print(f"Each person should pay: ${splitTipRound}")
 
                     # Second Attempt #
 
