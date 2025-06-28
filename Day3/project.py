dac3d26a16aed295d65da7eb2769f80034be79cd
Jake <jakechalmers86@gmail.com>
2025-06-28 21:04:01 +0800

    Project: Treasure Island (First Attempt)

--- Day3/project.py
+++ Day3/project.py
@@ -21,5 +21,29 @@ print('''
             | |_ _ __ ___  __ _ ___ _   _ _ __ ___ 
             | __| '__/ _ \/ _` / __| | | | '__/ _ \
             | |_| | |  __/ (_| \__ \ |_| | | |  __/
-             \__|_|  \___|\__,_|___/\__,_|_|  \___|
-''')
+             \__|_|  \___|\__,_|___/\__,_|_|  \___|''')
+
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.")
+
+direction = input("The road is narrow and windy through the thick jungle, suddenly you reach a crossroad, where do you want to go, left or right? ").lower()
+
+if direction == "left":
+    # Continue in game
+    swim = input("You approach lake at high tide, do you want to swim or wait? ").lower()
+    if swim == "wait":    
+        print("You waited and a boat arrived to take you across. You move forward in your quest!")
+    else:
+        print("You tried to swim and were eaten by a crocodile. Game Over.")
+    door = input("You see a castle in the distance, You reach the castle by nightfall and sneak inside. \n You walk down a dark staircase and enter a secret crypt, you are faced with 3 doors, one yellow, one red and one blue, one of the doors contains the treasure... \n Destiny awaits, which door do you choose? red, blue or yellow? ").lower()
+    if door == "yellow":
+        print("You Win, You found the treasure and escaped through a Trap door!")
+    elif door == "blue":
+        print("You are ripped apart by savage beasts, Game Over!") 
+    elif door == "red":
+        print("You are Burned alive by a raging inferno, Game Over!")
+    else:
+        print("You didn't enter a valid response, Game Over!")
+else:
+    print("You fell in a hole. Game Over.")
+  
