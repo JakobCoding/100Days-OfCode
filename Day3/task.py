01c9964e9d5ce5cb64a585152abca43ab875f3b9
Jake <jakechalmers86@gmail.com>
2025-06-28 19:01:51 +0800

    Day 3: Welcome to Python Pizza Service

--- Day3/task.py
+++ Day3/task.py
@@ -53,3 +53,27 @@ elif bmi <= 25:
     print("Congrats you weight is normal.. (according to science)")
 else:
     print("According to scienctific research algorithms you are overweight, sorry!")
+    
+                
+                    # PYTHON PIZZA SERVICE
+
+print("Welcome to Python Pizza Deliveries!")
+size = input("What size pizza do you want? S, M or L: ")
+pepperoni = input("Do you want pepperoni on your pizza? Y or N: ")
+extraCheese = input("Do you want extra cheese? Y or N: ")
+
+# todo: work out how much they need to pay based on their size choice.
+bill = 0
+if size == "S":
+    bill += 15
+elif size == "M":
+    bill += 20
+elif size == "L":
+    bill += 25
+else:
+    print("Sorry please enter S, M or L pizza size.")
+    
+# todo: work out how much to add to their bill based on their pepporoni choice    
+    
+
+        
