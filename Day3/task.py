ec01c2ee66483429c25c4f94318d89d9ee6f1b5e
Jake <jakechalmers86@gmail.com>
2025-06-28 17:49:46 +0800

    Day 3: Nested if else statement added to rollocoaster program

--- Day3/task.py
+++ Day3/task.py
@@ -2,8 +2,14 @@
 print("Welcome to the rollercoaster!")
 height = int(input("What is your height in cm? "))
 
+# Nested if Statements & elif statements
 if height >= 120:
     print("You can ride the rollercoaster") 
+    age = int(input("What is your age? "))
+    if age <= 18:
+        print("Please pay $7.")
+    else:
+        print("Please pay $12.")
 else: 
     print("Sorry you have to grow taller before you can ride.")    
     
@@ -15,8 +21,9 @@ print(10 % 3) # modulo = 1
 
 num1 = int(input("Enter a number: "))
  
-
 if num1 % 2 == 0: 
     print("Even Number!")
 else:
     print("Odd number!")
+    
+#
