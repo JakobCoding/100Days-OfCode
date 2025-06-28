11c79de0ac045a95f227f927644c351dc278f387
Jake <jakechalmers86@gmail.com>
2025-06-29 07:57:26 +0800

    Day2: Tip Calculator tweaks

--- Day2/project.py
+++ Day2/project.py
@@ -1,9 +1,8 @@
                 # Build a Tip Calculator 
-                    # First Attempt 
-
+                
 print("Welcome to the tip caluculator!")
-totalBill = input("What was the total bill? ")
-tip = input("How much tip would you like to give? 10, 12, or 15? $")
+totalBill = input("What was the total bill? $")
+tip = input("How much tip would you like to give? 10, 12, or 15? ")
 splitBetween = input("How many People to split the bill? ")
 
 floatBill = float(totalBill)
