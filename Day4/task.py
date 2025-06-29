d5d929821279b41e9af8636a7c2349878de7bc49
Jake <jakechalmers86@gmail.com>
2025-06-29 16:43:52 +0800

    Day4: Combining Lists and printing from index

--- Day4/task.py
+++ Day4/task.py
@@ -1,26 +1,27 @@
 # Randomisation - import random module
 import random
+import myModule
 
-# randomInt = random.randint(1, 100)
-# print(randomInt)
+randomInt = random.randint(1, 100)
+print(randomInt)
 
-# print(myModule.myFavoriteNumber)
+print(myModule.myFavoriteNumber)
 
 # Generating Floating point number in the range of 0.0 - < 1
-# randomNumber0_1 = random.random() * 10 # extend range past 1
-# print(randomNumber0_1)
+randomNumber0_1 = random.random() * 10 # extend range past 1
+print(randomNumber0_1)
 
-# # Generate Floating point numbers in a aspecified range
-# randomFloat = random.uniform(1, 100)
-# print(randomFloat)
+# Generate Floating point numbers in a aspecified range
+randomFloat = random.uniform(1, 100)
+print(randomFloat)
 
 # Random Head or Tails
 
-# headsOrTails = random.randint(0, 1)
-# if headsOrTails == 0:
-#     print("Heads")
-# else:
-#     print("Tails")
+headsOrTails = random.randint(0, 1)
+if headsOrTails == 0:
+     print("Heads")
+else:
+    print("Tails")
     
 # Lists - always start within square brackets seperated by a commer
 
@@ -39,3 +40,15 @@ print(random.choice(friends))
 # 2nd option
 randomIndex = random.randint(0, 4)
 print(friends[randomIndex])
+
+fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
+fruits[-1] = "Melons"
+fruits.append("Lemons")
+print(fruits)
+
+fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
+vegetables = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
+ 
+dirty_dozen = [fruits, vegetables]
+ 
+print(dirty_dozen[1][2])
