b1ff62fd71b4568c0fb89bb781c135e8d86e5af9
Jake <jakechalmers86@gmail.com>
2025-06-29 19:23:53 +0800

    Day 4: Rock Paper Scissors Project update

--- Day4/program.py
+++ Day4/program.py
@@ -1,1 +1,14 @@
 # Rock Paper Scissors Game #
+import random
+
+playerChoice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors? "))
+
+computerChoice = random.randint(0, 2)
+print(f"Computer chose {computerChoice}")
+
+if playerChoice == 0 and computerChoice == 2:
+    print("You win!")
+elif computerChoice > playerChoice:
+    print("You lose!")
+else: 
+    print("You typed an invalid number.")
