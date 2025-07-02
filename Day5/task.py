804ddc77a46fe54c83681da3c2b61e861dd7584e
Jake <jakechalmers86@gmail.com>
2025-07-03 07:57:37 +0800

    Day 5: How to add numbers in a list using sum()

--- Day5/task.py
+++ Day5/task.py
@@ -2,3 +2,9 @@
 fruits = ["Apple", "Peach", "Pear"]
 for fruit in fruits: 
     print(fruit)
+    
+# How to Add up numbers in a list
+
+student_scores = [150, 200, 199, 126, 186, 99, 55, 66, 88, 139]
+total_exam_scores = sum(student_scores)
+print(total_exam_scores)
