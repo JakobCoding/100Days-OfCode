7382f157e65e70f4683e230caea94ce4b1f81c6b
Jake <jakechalmers86@gmail.com>
2025-07-03 08:26:40 +0800

    Day 5: Finding a max value in list using for loop

--- Day5/task.py
+++ Day5/task.py
@@ -3,16 +3,29 @@ fruits = ["Apple", "Peach", "Pear"]
 for fruit in fruits: 
     print(fruit)
     
-# How to Add up numbers in a list
+# How to Add up numbers in a list using sum() function
 
-student_scores = [150, 200, 199, 126, 186, 99, 55, 66, 88, 139]
+student_scores = [150, 199, 126, 186, 99, 55, 66, 88, 139, 200]
 total_exam_scores = sum(student_scores)
 print(total_exam_scores) # console = 1308
 
-# How to use a for loop to create a sum from a list
+# How to use a for loop to create a sum from list items
 
 sum = 0
 for score in student_scores:
     sum += score
     
 print(sum) # console = 1308
+
+# Use Max function to print the max value in a list
+
+print(max(student_scores)) # console = 200 
+
+# finding Max value in a list using a for loop
+
+max_score = 0 # Initilise max_score to 0 
+for score in student_scores:
+    if score > max_score:
+        max_score = score
+
+print(max_score) # console = 200 
