92a6aa9c3a2ae7f5c5f329ae7ff7f40699a15f64
Jay Cee <jakechalmers86@gmail.com>
2025-07-03 11:33:39 +0800

    Day 5: Using range() function & for loop add sum of numbers between 1 and 100

--- Day5/task.py
+++ Day5/task.py
@@ -1,31 +1,43 @@
-# For Loops
-fruits = ["Apple", "Peach", "Pear"]
-for fruit in fruits: 
-    print(fruit)
+# # For Loops
+# fruits = ["Apple", "Peach", "Pear"]
+# for fruit in fruits: 
+#     print(fruit)
     
-# How to Add up numbers in a list using sum() function
+# # How to Add up numbers in a list using sum() function
 
-student_scores = [150, 199, 126, 186, 99, 55, 66, 88, 139, 200]
-total_exam_scores = sum(student_scores)
-print(total_exam_scores) # console = 1308
+# student_scores = [150, 199, 126, 186, 99, 55, 66, 88, 139, 200]
+# total_exam_scores = sum(student_scores)
+# print(total_exam_scores) # console = 1308
 
-# How to use a for loop to create a sum from list items
+# # How to use a for loop to create a sum from list items
 
-sum = 0
-for score in student_scores:
-    sum += score
+# sum = 0
+# for score in student_scores:
+#     sum += score
     
-print(sum) # console = 1308
+# print(sum) # console = 1308
 
-# Use Max function to print the max value in a list
+# # Use Max function to print the max value in a list
 
-print(max(student_scores)) # console = 200 
+# print(max(student_scores)) # console = 200 
 
-# finding Max value in a list using a for loop
+# # finding Max value in a list using a for loop
 
-max_score = 0 # Initialise max_score to 0 
-for score in student_scores:
-    if score > max_score:
-        max_score = score
+# max_score = 0 # Initialise max_score to 0 
+# for score in student_scores:
+#     if score > max_score:
+#         max_score = score
 
-print(max_score) # console = 200 
+# print(max_score) # console = 200 
+
+# Using Range with a for loop
+for number in range(1, 11, 3):
+    print(number)
+
+# Add up all the numbers from 1 to 100 using a for loop
+total = 0 
+for number in range(1, 101):
+    total += number
+    print(total)
+    
+print(total)
