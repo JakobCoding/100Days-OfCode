b5f50270892c2e1b89dc6748368e483843eaf7d4
Jake <jakechalmers86@gmail.com>
2025-07-03 08:08:03 +0800

    Day 5: Use for loop to create a sum of a list

--- Day5/task.py
+++ Day5/task.py
@@ -7,5 +7,12 @@ for fruit in fruits:
 
 student_scores = [150, 200, 199, 126, 186, 99, 55, 66, 88, 139]
 total_exam_scores = sum(student_scores)
-print(total_exam_scores)
+print(total_exam_scores) # console = 1308
 
+# How to use a for loop to create a sum from a list
+
+sum = 0
+for score in student_scores:
+    sum += score
+    
+print(sum) # console = 1308
